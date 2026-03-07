c139696a9f35d9c9bc0c2d7a68741acb230b12ac
rhett66666 <rhett6666@gmail.com>
2026-03-07 20:14:45 +0800

    Update main.py

--- main.py
+++ main.py
@@ -17,6 +17,9 @@ from google.oauth2 import service_account
 from googleapiclient.discovery import build
 from googleapiclient.http import MediaFileUpload, MediaIoBaseDownload
 
+import functools
+print = functools.partial(print, flush=True)
+
 warnings.filterwarnings("ignore", message="Unverified HTTPS request")
 
 # === 1. 權限與路徑設定 ===
@@ -587,4 +590,5 @@ def main():
         conn.close()
         sync_db_to_cloud(db_cloud_id)
 if __name__ == "__main__":
+    print("🚩 進入 __main__ 區塊")
     main()
